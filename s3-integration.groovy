d47b9074063e4437aa0d44c1b2c1a70afc522c1b
nikiani <nikianiautomation@gmail.com>
2025-09-16 20:13:04 +0530

    added git clone

--- s3-integration.groovy
+++ s3-integration.groovy
@@ -2,30 +2,25 @@ pipeline {
     agent any
     stages {
         stage('pull from git') {
-          steps {
-
-    
-          echo 'calling Repo'
-          sh "rm -rf PROJECT1"
+            steps {
+                echo 'calling Repo'
+                sh "rm -rf PROJECT1"
                 sh "git clone https://github.com/nikianiautomation/PROJECT1.git"
-                sh "pwd"
-                
-                echo 'End'  
-          }
+                echo 'End'
+            }
         }
         stage('Upload to S3') {
             steps {
-               dir('PROJECT1/s3') {
-                sh "pwd"
-                withAWS(credentials: 'niki-s3', region: 'us-east-2') {
-                s3Upload(
-                    bucket: 'niki-ani1',
-                    file: 's3-terraform.tf',
-                    path: 'my-app2/'
-                )
+                dir('PROJECT1/s3') {
+                    withAWS(credentials: 'niki-s3', region: 'us-east-2') {
+                        s3Upload(
+                            bucket: 'niki-ani1',
+                            file: 's3-terraform.tf',
+                            path: 'my-app2/'
+                        )
+                    }
+                }
             }
-               }
-        }
         }
     }
 }
