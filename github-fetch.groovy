4c7df36a0b1d93413043e9d3c57487a6cac7fb13
nikiani <nikianiautomation@gmail.com>
2025-09-16 17:32:31 +0530

    added sh to wget

--- github-fetch.groovy
+++ github-fetch.groovy
@@ -5,7 +5,7 @@ pipeline {
             steps {
                 echo 'calling Repo'
                 sh "wget -O s3-terraform.tf https://github.com/nikianiautomation/PROJECT1/blob/main/s3/s3-terraform.tf"
-                cat s3-terraform.tf
+                sh "cat s3-terraform.tf"
                 echo 'End'
             }
         }
