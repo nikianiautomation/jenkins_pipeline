dfd8a2b25b831afa0d062b9652e216664223c597
nikiani <nikianiautomation@gmail.com>
2025-09-16 19:35:18 +0530

    added git pull functionality

--- s3-integration.groovy
+++ s3-integration.groovy
@@ -1,13 +1,19 @@
 pipeline {
     agent any
     stages {
+        stage('pull from git'){
+          echo 'calling Repo'
+                sh "wget -O s3-terraform.tf https://github.com/nikianiautomation/PROJECT1/blob/main/s3/s3-terraform.tf"
+                
+                echo 'End'  
+        }
         stage('Upload to S3') {
             steps {
-                sh 'echo "Hello with full S3 Publisher config" > hello.txt'
+                
                 withAWS(credentials: 'niki-s3', region: 'us-east-2') {
                 s3Upload(
                     bucket: 'niki-ani1',
-                    file: 'hello.txt',
+                    file: 's3-terraform.tf',
                     path: 'my-app/'
                 )
             }
