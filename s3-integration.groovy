c68782ddfbe4fd64f404d35f66f727a0148bb449
nikiani <nikianiautomation@gmail.com>
2025-09-16 17:51:56 +0530

    added groovy script for s3 integration

--- s3-integration.groovy
+++ s3-integration.groovy
@@ -5,11 +5,13 @@
                         steps {
                             // Your build steps to generate artifacts
                             sh 'echo "This is a test artifact." > artifact.txt'
+                            sh "pwd"
                         }
                     }
                     stage('Upload to S3') {
                         steps {
                             script {
+                                sh "pwd"
                                 s3Upload(file: 'artifact.txt', bucket: 'niki-ani3', path: 'artifacts/build-artifacts/artifact.txt')
                             }
                         }
