70ac46d216799a8687fef18040a174c8e545d647
nikiani <nikianiautomation@gmail.com>
2025-09-16 18:24:56 +0530

    added groovy script for s3 credentials

--- s3-integration.groovy
+++ s3-integration.groovy
@@ -1,19 +1,15 @@
-            pipeline {
-              agent any
-              stages {
-                stage('Build') {
-                  steps {
-                    // Your build steps to generate artifacts
-                    sh 'echo "This is a test artifact." > artifact.txt'
-                    sh "pwd"
-                  }
+pipeline {
+    agent any
+    stages {
+        stage('Upload to S3') {
+            steps {
+                withAWS(credentials: 'AKIA6HMLXTAQUBOYVI4Y', region: 'us-east-2') {
+                    // Create a sample file (optional)
+                    sh 'echo "Hello from Jenkins" > hello.txt'
+                    // Upload the file to your S3 bucket
+                    s3Upload(bucket: 'niki-ani1', file: 'hello.txt', path: 'hello.txt')
                 }
-                stage("Upload") {
-                  steps {
-                    withAWS(region: "us-east-2", credentials: "AKIA6HMLXTAQUBOYVI4Y"){
-                      s3Upload(entries: [[file: "artifact.txt", s3Prefix: "artifact/path/", bucket: "niki-ani1"]] )
-                    }
-                  }
-                }
-              }
             }
+        }
+    }
+}
