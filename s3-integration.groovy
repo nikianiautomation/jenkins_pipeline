f284347f11d7d011f0fc5deb52d507b3dd5fe7f9
nikiani <nikianiautomation@gmail.com>
2025-09-16 18:02:07 +0530

    added groovy script for s3 credentials

--- s3-integration.groovy
+++ s3-integration.groovy
@@ -1,20 +1,22 @@
             pipeline {
-                agent any
-                stages {
-                    stage('Build') {
-                        steps {
-                            // Your build steps to generate artifacts
-                            sh 'echo "This is a test artifact." > artifact.txt'
-                            sh "pwd"
-                        }
-                    }
-                    stage('Upload to S3') {
-                        steps {
-                            script {
-                                sh "pwd"
-                                s3Upload(file: 'artifact.txt', bucket: 'niki-ani1', path: 'artifact.txt')
-                            }
-                        }
-                    }
+              agent any
+              stages {
+                stage('Build') {
+                  steps {
+                    // Your build steps to generate artifacts
+                    sh 'echo "This is a test artifact." > artifact.txt'
+                    sh "pwd"
+                  }
                 }
-            }
+                stage('Upload to S3') {
+                  steps {
+                    withAWS(credentials: "AKIA6HMLXTAQUBOYVI4Y", region: "us-east-2") {
+                      s3Upload(
+                        file: "artifact.txt",
+                        bucket: "niki-ani1",
+                        path: "artifact.txt", // Optional: path within the S3 bucket
+                        acl: 'Private' // Example: 'PublicRead', 'Private', etc.
+                      )
+                    }
+                  }
+                }
