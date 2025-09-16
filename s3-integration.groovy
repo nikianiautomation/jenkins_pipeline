13c8aac96979de445a8830244a77f8abff2acb12
nikiani <nikianiautomation@gmail.com>
2025-09-16 18:04:46 +0530

    added groovy script for s3 credentials

--- s3-integration.groovy
+++ s3-integration.groovy
@@ -8,16 +8,17 @@
                     sh "pwd"
                   }
                 }
-                stage('Upload to S3') {
-                  steps {
-                    withAWS(credentials: "AKIA6HMLXTAQUBOYVI4Y", region: "us-east-2") {
-                      s3Upload(
-                        file: "artifact.txt",
-                        bucket: "niki-ani1",
-                        path: "artifact.txt", // Optional: path within the S3 bucket
-                        acl: 'Private' // Example: 'PublicRead', 'Private', etc.
-                      )
-                    }
+              stage('Upload to S3') {
+                steps {
+                  withAWS(credentials: "AKIA6HMLXTAQUBOYVI4Y", region: "us-east-2") {
+                    s3Upload(
+                      file: "artifact.txt",
+                      bucket: "niki-ani1",
+                      path: "artifact.txt", // Optional: path within the S3 bucket
+                      acl: 'Private' // Example: 'PublicRead', 'Private', etc.
+                    )
                   }
                 }
               }
+            }
+        }
