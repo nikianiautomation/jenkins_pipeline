bc1834e6ce85af9ecdb86f335a494e9e07b683b5
nikiani <nikianiautomation@gmail.com>
2025-09-16 19:26:21 +0530

    added groovy script for s3 credentials

--- s3-integration.groovy
+++ s3-integration.groovy
@@ -6,18 +6,9 @@ pipeline {
                 sh 'echo "Hello with full S3 Publisher config" > hello.txt'
                 withAWS(credentials: 'niki-s3', region: 'us-east-2') {
                 s3Upload(
-                    entries: [
-                        [bucket: 'niki-ani1', sourceFile: 'hello.txt', target: 'hello.txt']
-                    ],
-                    userMetadata: [                                         // Required metadata key-value pairs
-                        [key: 'uploadedBy', value: 'jenkins'],
-                        [key: 'buildNumber', value: "${env.BUILD_NUMBER}"]
-                    ],
-                    profileName: 'AWS_DEFAULT_PROFILE',
-                    dontWaitForConcurrentBuildCompletion: false,          // Boolean, usually false
-                    consoleLogLevel: 'INFO',                               // Options: INFO, WARN, ERROR, DEBUG
-                    pluginFailureResultConstraint: 'FAILURE',             // Action on failure, e.g., FAILURE
-                    dontSetBuildResultOnFailure: false                     // Whether to continue build on failure
+                    bucket: 'niki-ani1',
+                    file: 'hello.txt',
+                    path: 'my-app/'
                 )
             }
         }
