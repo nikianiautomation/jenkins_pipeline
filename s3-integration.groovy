98251395f27a2255daa5c10f2edfd7b258a705d5
nikiani <nikianiautomation@gmail.com>
2025-09-16 19:14:51 +0530

    added groovy script for s3 credentials

--- s3-integration.groovy
+++ s3-integration.groovy
@@ -6,9 +6,6 @@ pipeline {
                 sh 'echo "Hello with full S3 Publisher config" > hello.txt'
                 withAWS(credentials: 'niki-s3', region: 'us-east-2') {
                 s3Upload(
-                    bucket: 'niki-ani1',
-                    file: 'hello.txt',
-                    path: 'my-app/'
                     entries: [
                         [bucket: 'niki-ani1', sourceFile: 'hello.txt', target: 'hello.txt']
                     ],
